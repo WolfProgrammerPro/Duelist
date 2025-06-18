2fa78286f1125a3a82c89cc28e9971948606f1b9
WolfProgrammerPro <161871668+WolfProgrammerPro@users.noreply.github.com>
2025-06-18 16:35:36 +0500

    Load Main.js

---
+++ main.js
@@ -0,0 +1,47 @@
+const isMobile = window.innerWidth < 768
+
+if (isMobile){
+    const img2 = document.getElementById("img2")
+    img2.style.top =  '125%'
+    const img3 = document.getElementById("img3")
+    img3.style.top =  '170%'
+    const img4 = document.getElementById("img4")
+    img4.style.top =  '215%'
+    const upr = document.getElementById("upr")
+    upr.style.textAlign = 'center'
+    upr.style.width = '100%'
+    upr.style.top = '265%'
+    upr.style.left = '0%'
+    const lkm = document.getElementById("lkm")
+    lkm.style.top = '270%'
+    const pkm = document.getElementById("pkm")
+    pkm.style.top = '275%'
+    pkm.style.left = '5%'
+    const q = document.getElementById("q")
+    q.style.top = '280%'
+    q.style.left = '5%'
+    const e = document.getElementById("e")
+    e.style.top = '285%'
+    e.style.left = '5%'
+    const poloska = document.getElementById("poloska")
+    poloska.style.top = '290%'
+    poloska.style.left = '5%'
+    poloska.style.textAlign = 'center'
+    poloska.style.width = '90%'
+    const def = document.getElementById("defend")
+    def.style.top = '295%'
+    def.style.left = '5%'
+    def.style.textAlign = 'center'
+    def.style.width = '90%'
+    const studio = document.getElementById("studio")
+    studio.style.top = '315%'
+    const unity = document.getElementById("unity")
+    unity.style.top = '305%'
+    unity.style.width = '90%'
+    unity.style.left = '5%'
+    const zs = document.getElementById("zs")
+    zs.style.top = '310%'
+    zs.style.width = '90%'
+    zs.style.left = '5%'
+    
+}
